471001f5bd55c47d85fd0d3f15fce8a431ff5424
Zumori <BohdanCh.w@gmail.com>
2022-05-30 18:58:40 +0300

    Update cors middlware

--- web/middleware/cors.go
+++ web/middleware/cors.go
@@ -11,15 +11,21 @@ import (
 type CoorsMid struct {
 	AllowAll       bool
 	AllowedOrigins set.StringSet
+	AllowedMethods set.StringSet
 }
 
 func (mid *CoorsMid) Wrap(h api.Handler) api.Handler {
 	f := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		if mid.AllowAll {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		} else {
 			if mid.AllowedOrigins.Contains(r.Host) {
 				w.Header().Set("Access-Control-Allow-Origin", r.Host)
+			}
+
+			if mid.AllowedMethods.Contains(r.Method) {
+				w.Header().Set("Access-Control-Allow-Methods", r.Method)
 			}
 		}
 
